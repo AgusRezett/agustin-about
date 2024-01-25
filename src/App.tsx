dc97c55014c2ac21fb62fe3ede0b31102d6e10fc
Agustin Rezett <agustin.rezett@gmail.com>
2024-01-25 00:15:59 -0300

    feat: toggle theme animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Header from './components/organisms/Header/Header';
 import MainPage from './components/pages/MainPage/MainPage';
 import { toggleTheme } from './utilities/themes';
 
 const App = () => {
+	const [scanning, setScanning] = useState(false);
+
 	return (
 		<div className={`app`}>
-			<button onClick={toggleTheme}>Change</button>
+			<button onClick={() => toggleTheme(setScanning)}>Change</button>
+			{scanning && <div className={`Scanner ${scanning ? 'ScannerAnimation' : ''}`}></div>}
 			<Header />
 			<MainPage />
 		</div>
--- src/utilities/themes.ts
+++ src/utilities/themes.ts
@@ -1,5 +1,12 @@
-export const toggleTheme = () => {
+import { SetStateAction } from 'react';
+
+export const toggleTheme = (setScanning: { (value: SetStateAction<boolean>): void; (arg0: boolean): void }) => {
+	setScanning(true);
 	const currentTheme = document.documentElement.getAttribute('data-theme');
 	const theme = currentTheme === 'theme-light' ? 'theme-dark' : 'theme-light';
+
 	document.documentElement.setAttribute('data-theme', theme);
+	setTimeout(() => {
+		setScanning(false);
+	}, 1000);
 };
