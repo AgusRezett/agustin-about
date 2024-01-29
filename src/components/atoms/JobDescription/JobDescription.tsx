713909f4a12736721b5af76c4597cb6006835c9c
Agustin Rezett <agustin.rezett@gmail.com>
2024-01-29 00:01:50 -0300

    feat: update content

--- src/components/atoms/JobDescription/JobDescription.tsx
+++ src/components/atoms/JobDescription/JobDescription.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import style from './JobDescription.module.scss';
 
 interface IJobDescriptionProps {
 	description: string;
 }
 
 const JobDescription = ({ description }: IJobDescriptionProps) => {
-	return <p>{description}</p>;
+	const isList = description.includes(`/s`);
+	return !isList ? (
+		<p>{description}</p>
+	) : (
+		<ul className={style.List}>
+			{description.split(`/s`).map((item) => (
+				<li key={item} className={style.List__Item}>
+					<span className={style.List__Item__Point}></span>
+					<div>
+						<span className={style.List__Item__FirstWord}>{item.split(' ')[0]} </span>
+						<span>{item.split(' ').slice(1).join(' ')}</span>
+					</div>
+				</li>
+			))}
+		</ul>
+	);
 };
 
 export default JobDescription;
