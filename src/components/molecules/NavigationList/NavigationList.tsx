3aa76616c823588ca991578a083e47a9b1ec12b9
Agustin Rezett <agustin.rezett@gmail.com>
2024-02-01 21:56:06 -0300

    feat: add back to top navigation

--- src/components/molecules/NavigationList/NavigationList.tsx
+++ src/components/molecules/NavigationList/NavigationList.tsx
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
 import style from './NavigationList.module.scss';
 import { LanguageContext } from 'src/hooks/useLanguageContext';
+import { IoArrowUp, IoArrowUpCircle, IoLogoGithub } from 'react-icons/io5';
 
 const NavigationList = () => {
-	const { content } = useContext(LanguageContext) as ILanguageContext;
+	const { content, language } = useContext(LanguageContext) as ILanguageContext;
 
 	const handleScroll = (sectionId: number) => {
 		const elementId = `content-section-${sectionId}`;
@@ -18,7 +19,7 @@ const NavigationList = () => {
 			{content.MenuNavigationItems.map((item) => (
 				<li
 					key={`${item.value}`}
-					className={`${style.NavigationItem} ${item.value === 1 ? style.NavigationItemActive : ''}`}
+					className={style.NavigationItem}
 					onClick={() => {
 						handleScroll(item.value);
 					}}
@@ -26,6 +27,15 @@ const NavigationList = () => {
 					{item.label}
 				</li>
 			))}
+			<li
+				className={style.NavigationItem}
+				onClick={() => {
+					window.scrollTo({ top: 0, behavior: 'smooth' });
+				}}
+			>
+				{language === 'en' ? 'Back to top' : 'Volver arriba'}
+				<IoArrowUpCircle size={20} />
+			</li>
 		</ul>
 	);
 };
